7d634176dcc9b0f869649ac2cb4fe91cadb216b7
risa.mehara <>
2019-05-10 15:57:20 +0900

    created display for calculator activity1

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/calculator/CalculatorActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/calculator/CalculatorActivity.kt
@@ -1,10 +1,9 @@
 package com.example.android.architecture.blueprints.todoapp.calculator
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.architecture.blueprints.todoapp.R
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import com.example.android.architecture.blueprints.todomvvmlivekotlin.mock.R
-
 import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : AppCompatActivity() {
@@ -14,10 +13,10 @@ class CalculatorActivity : AppCompatActivity() {
         setContentView(R.layout.activity_calculator)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+        /*fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
-        }
+        }*/
     }
 
 }
