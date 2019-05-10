3811ab3a1b42b7b5d4013768d844f37dbfcd8109
risa.mehara <>
2019-05-10 19:08:24 +0900

    added layout for CalcResult page

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/calculator/CalcResultActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/calculator/CalcResultActivity.kt
@@ -14,10 +14,8 @@ class CalcResultActivity : AppCompatActivity() {
         setContentView(R.layout.activity_calc_result)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
+
     }
 
 }
