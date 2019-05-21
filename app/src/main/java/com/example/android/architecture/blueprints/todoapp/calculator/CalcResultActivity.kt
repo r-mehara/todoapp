35fa566753c00efc2a7d3c81619399b557fa6fc9
risa.mehara <>
2019-05-21 11:02:58 +0900

    fix calcResult select image function from if to when

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/calculator/CalcResultActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/calculator/CalcResultActivity.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.architecture.blueprints.todoapp.R
 import kotlinx.android.synthetic.main.activity_calc_result.*
+import kotlinx.android.synthetic.main.content_calc_result.*
 
 
 class CalcResultActivity : AppCompatActivity() {
@@ -18,19 +19,20 @@ class CalcResultActivity : AppCompatActivity() {
         val text = intent.getStringExtra("CALC_RESULT")
         val calcView = findViewById<TextView>(R.id.calc_result)
         calcView.text = text
-        val calcResult: Double = text.toDouble()
+        val n: Double = text.toDouble()
 
-        if (calcResult%2==0.0){
-            val imageEven = findViewById<ImageView>(R.id.imgView)
-            imageEven.setImageResource(R.drawable.even)
-        }else if(calcResult%2==1.0){
-            val imageOdd = findViewById<ImageView>(R.id.imgView)
-            imageOdd.setImageResource(R.drawable.odd)
-        }else{
-            val imageOther = findViewById<ImageView>(R.id.imgView)
-            imageOther.setImageResource(R.drawable.other)
+        imgView.setImageResource(selectImage(n))
+    }
+
+    private fun selectImage (calcResult: Double): Int{
+
+        return when{
+
+            calcResult%2==0.0 -> R.drawable.even
+            calcResult%2==1.0 -> R.drawable.odd
+            else -> R.drawable.other
+
         }
-
     }
 
 }
