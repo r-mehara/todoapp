cb0329a29924a6a36ed0cab2190b05159d6982b2
risa.mehara <>
2019-05-21 11:11:32 +0900

    add bottom constraint to calcResult layout

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/calculator/CalcResultActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/calculator/CalcResultActivity.kt
@@ -1,7 +1,6 @@
 package com.example.android.architecture.blueprints.todoapp.calculator
 
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.architecture.blueprints.todoapp.R
